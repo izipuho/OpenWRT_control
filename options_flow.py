984c5e309aa8576ddee1463f3fd058221a3a57b9
izipuho <58221294+izipuho@users.noreply.github.com>
2025-05-19 12:37:30 +0300

    Update options_flow.py

--- options_flow.py
+++ options_flow.py
@@ -1,27 +1,30 @@
-import voluptuous as vol
-
 from homeassistant import config_entries
-from homeassistant.core import callback
+import voluptuous as vol
+from .const import DOMAIN, CONF_CONFIG_TYPE
+import yaml
+import os
 
-from .const import DOMAIN, CONF_IP, CONF_CONFIG_TYPE
-from .config_flow import load_config_types
+CONFIG_PATH = "/config/custom_components/openwrt_updater/config_types.yaml"
 
+def load_config_types():
+    if os.path.exists(CONFIG_PATH):
+        with open(CONFIG_PATH, "r") as file:
+            config = yaml.safe_load(file)
+            return {entry["name"]: entry["id"] for entry in config}
+    return {}
 
-class OpenWRTOptionsFlow(config_entries.OptionsFlow):
+class OpenWRTOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
-        errors = {}
         config_types = load_config_types()
-        config_type_names = [item["name"] for item in config_types]
-
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        schema = vol.Schema({
-            vol.Required(CONF_IP, default=self.config_entry.data.get(CONF_IP)): str,
-            vol.Required(CONF_CONFIG_TYPE, default=self.config_entry.data.get(CONF_CONFIG_TYPE)): vol.In(config_type_names),
-        })
-
-        return self.async_show_form(step_id="init", data_schema=schema, errors=errors)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional(CONF_CONFIG_TYPE, default=self.config_entry.data.get(CONF_CONFIG_TYPE)): vol.In(list(config_types.keys())),
+            })
+        )
