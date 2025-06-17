9e76d7e2b9ed793dad6f25016467be5cee0f5a4d
izipuho <58221294+izipuho@users.noreply.github.com>
2025-06-17 15:04:42 +0300

    one last const eliminated

--- custom_components/openwrt_updater/const.py
+++ custom_components/openwrt_updater/const.py
@@ -1,8 +1,6 @@
 """Constants and helpers."""
 
 DOMAIN = "openwrt_updater"
-CONFIG_TYPES_PATH = "custom_components/openwrt_updater/config_types.yaml"
-
 
 def get_device_info(ip: str) -> dict:
     """Return device info for all platforms based on IP."""
