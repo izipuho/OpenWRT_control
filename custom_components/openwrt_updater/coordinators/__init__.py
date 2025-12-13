79d228ad21f1c8f160f4b1665b80ce23dde188ae
izipuho <58221294+izipuho@users.noreply.github.com>
2025-12-13 10:00:23 +0300

    remove old coordinator import

--- custom_components/openwrt_updater/coordinators/__init__.py
+++ custom_components/openwrt_updater/coordinators/__init__.py
@@ -2,10 +2,8 @@
 
 from .device import OpenWRTDeviceCoordinator
 from .sysupgrade import LocalTohCacheCoordinator
-from .toh import TohCacheCoordinator
 
 __all__ = [
     "LocalTohCacheCoordinator",
     "OpenWRTDeviceCoordinator",
-    "TohCacheCoordinator",
 ]
